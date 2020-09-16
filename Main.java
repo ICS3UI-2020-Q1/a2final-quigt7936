e522a96bf4b1fd4231424b39f2e68d1368852a6c
Thomas Quigley <quigt7936@wrdsb.ca>
2020-09-16 12:54:41 +0000

    Completed summative

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program tells you the price of a pizza.
+ * @author Thomas Quigley 
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
     
+    // ask and store the diameter of the pizza.
+    System.out.println("What is the size of the pizza (in cm):");
+    int pizzaDiameter = input.nextInt();
+
+    // declare the variables and constants
+    double diameterCost = 0.50 * pizzaDiameter;
+    final double LABOUR_COST = 0.75;
+    final double OVEN_COST = 0.99;
+    final double TAX = 0.13;
+
+    // find the costs
+    double subTotal = LABOUR_COST + diameterCost + OVEN_COST;
+    double taxTotal = subTotal * TAX;
+    double total = subTotal + taxTotal;
+
+    // tell them their costs
+    System.out.println("Subtotal: $" + subTotal);
+    System.out.println("Taxes: $" + taxTotal);
+    System.out.println("Total: $" + total);
+
+    // find out what message we give them, and give it to them
+    if (pizzaDiameter >= 1 && pizzaDiameter < 20) {
+      System.out.println("We are going to make you a cute little pizza!");
+    } else if (pizzaDiameter >= 20 && pizzaDiameter < 40) {
+      System.out.println("This will be delicious!");
+    } else {
+      System.out.println("Whoa, big pizza! You might need a truck to get this home!");
+    }
   }
 }
